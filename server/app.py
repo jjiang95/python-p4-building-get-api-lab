cc33f4b13dcfb0bc9d300fe91cb7b8ef853158ee
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-09-05 16:21:51 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,51 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries_dict = []
+    for bakery in Bakery.query.all():
+        bakeries_dict.append(bakery.to_dict())
+           
+    response = make_response(
+        jsonify(bakeries_dict),
+        200,
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    goods_dict = []
+    for good in BakedGood.query.order_by(BakedGood.price.desc()).all():
+        goods_dict.append(good.to_dict())
+
+    response = make_response(
+        jsonify(goods_dict),
+        200
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    
+    response = make_response(
+        jsonify(good.to_dict()),
+        200
+    )
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
